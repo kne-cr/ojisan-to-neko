0d770c00e44733ee287973725ef2d29d7302ce7d
kne-cr <yes6sss1@gmail.com>
2021-08-05 21:59:16 +0900

    移動と勝利条件を追加

--- js/script.js
+++ js/script.js
@@ -1,24 +1,94 @@
 (() => {
+  class Piece {
+    constructor(player, size) {
+      this.player = player;
+      this.size = size;
+    }
+  }
+
+  class Cell {
+    constructor() {
+      this.pieces = [];
+    }
+
+    player() {
+      if (!this.pieces.length) return 0;
+      return this.pieces[this.pieces.length - 1].player;
+    }
+  }
+
+  class Board {
+    constructor() {
+      this.cells = Array(3);
+      for (let col = 0; col < this.cells.length; col++) {
+        this.cells[col] = [];
+        for (let row = 0; row < 3; row++) {
+          this.cells[col][row] = new Cell();
+        }
+      }
+    }
+
+    win_horizontal(player) {
+      return [0,1,2].some(col => {
+        return [0,1,2].every(row => {
+          return this.cells[col][row].player() == player;
+        });
+      });
+    }
+
+    win_vertical(player) {
+      return [0,1,2].some(row => {
+        return [0,1,2].every(col => {
+          return this.cells[col][row].player() == player;
+        });
+      });
+    }
+
+    win_diagonal(player) {
+      if(
+        this.cells[0][0].player() == player
+        && this.cells[1][1].player() == player
+        && this.cells[2][2].player() == player
+      ) return true;
+      if(
+        this.cells[0][2].player() == player
+        && this.cells[1][1].player() == player
+        && this.cells[2][0].player() == player
+      ) return true;
+
+      return false;
+    }
+
+    win(player) {
+      if(this.win_vertical(player)) return true;
+      if(this.win_horizontal(player)) return true;
+      if(this.win_diagonal(player)) return true;
+
+      return false;
+    }
+  }
+
   const pieces = Array.from(document.getElementsByClassName("piece"));
   pieces.forEach(piece => {
     piece.addEventListener("dragstart", (event) => {
       console.log("dragstart");
       event.dataTransfer.setData('piece_id', event.target.id);
     });
-    piece.addEventListener("drop", (event) => {
-      event.preventDefault();
-    });
   });
+
   const cells = Array.from(document.getElementsByClassName("board__cell"));
   cells.forEach(cell => {
     cell.addEventListener("dragover", (event) => {
       event.preventDefault();
     });
     cell.addEventListener("drop", (event) => {
-      console.log("drop");
-      console.log(event.target.closest(".board__cell"));
-      event.target.closest(".board__cell").appendChild(document.getElementById(event.dataTransfer.getData('piece_id')));
+      const piece = document.getElementById(event.dataTransfer.getData('piece_id'));
+      const cell = event.target.closest(".board__cell");
+      cell.appendChild(piece);
+      globalThis.board.cells[cell.dataset.y][cell.dataset.x].pieces.push(new Piece(piece.dataset.player, 3));
       event.preventDefault();
     });
   });
+
+  globalThis.board = new Board();
 })();
