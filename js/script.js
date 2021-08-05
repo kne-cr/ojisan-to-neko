024775bca96cd129a02100637a5ba1bb006adc48
kne-cr <yes6sss1@gmail.com>
2021-08-05 19:48:02 +0900

    基本的な動きを作成

--- js/script.js
+++ js/script.js
@@ -1,14 +1,24 @@
 (() => {
   let pieces = Array.from(document.getElementsByClassName("piece"));
   pieces.forEach(piece => {
-    piece.addEventListener("mousedown", (event) => {
-      alert("mousedown");
+    piece.addEventListener("dragstart", (event) => {
+      console.log("dragstart");
+      event.dataTransfer.setData('piece_id', event.target.id);
     });
-    // piece.addEventListener("mouseup", (event) => {
-    //   alert("mouseup");
-    // });
-    // piece.addEventListener("dragstart", () => {
-    //   return false;
-    // });
+    piece.addEventListener("drop", (event) => {
+      event.preventDefault();
+    });
+  });
+  let cells = Array.from(document.getElementsByClassName("board__cell"));
+  cells.forEach(cell => {
+    cell.addEventListener("dragover", (event) => {
+      event.preventDefault();
+    });
+    cell.addEventListener("drop", (event) => {
+      console.log("drop");
+      console.log(event.target.closest(".board__cell"));
+      event.target.closest(".board__cell").appendChild(document.getElementById(event.dataTransfer.getData('piece_id')));
+      event.preventDefault();
+    });
   });
 })();
